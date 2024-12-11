f2fa74274f38d8dc9a529f5ee0763f9b00ac3242
amaan-p <amaanmpatelemail@gmail.com>
2024-12-11 21:12:01 +0530

    validation added

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ mongoose.connect(url)
   })
 
 
+
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
@@ -34,36 +36,61 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end() 
+      }
+    })
+    .catch(error => {
+      console.log(error)
+
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
   
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (body.content === undefined) {
-      return response.status(400).json({ error: 'content missing' })
-    }
-  
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
-    })
-  
-    note.save().then(savedNote => {
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save()
+    .then(savedNote => {
       response.json(savedNote)
     })
+
+    .catch(error => next(error))
+})
+
+  app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
   })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
+  app.put('/api/notes/:id', (request, response, next) => {
+
+    const { content, important } = request.body
   
-    response.status(204).end()
+    Note.findByIdAndUpdate(
+      request.params.id, 
+  
+      { content, important },
+      { new: true, runValidators: true, context: 'query' }
+    ) 
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
   })
-
 
 const PORT =process.nextTick.PORT||process.env.PORT
 app.listen(PORT, () => console.log("server running"));
